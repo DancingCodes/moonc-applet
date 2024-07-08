fed01302355a13a62a8f3399e39ea97c3fae77d3
张长悦 <dancingcodes@qq.com>
2024-07-08 13:35:25 +0800

    更新

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-App()
+App({
+    onLaunch() {
+        wx.getUpdateManager().onUpdateReady(() => {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success: (res) => {
+                    if (res.confirm) {
+                        updateManager.applyUpdate()
+                    }
+                }
+            })
+        })
+    }
+})
