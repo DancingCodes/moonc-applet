af3f0cdcf637f5fb3b1bcfd0a967abaa8b9a3185
张长悦 <dancingcodes@qq.com>
2024-07-08 15:06:05 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,15 @@
-Page({})
+Page({
+    data: {
+        show: false
+    },
+    addItem: function () {
+        this.setData({
+            show: true
+        })
+    },
+    hideOverlay: function () {
+        this.setData({
+            show: false
+        })
+    }
+})
