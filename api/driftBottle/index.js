1be8f4a1c681786b552a65b0b1cf4f2ce17c4411
张长悦 <dancingcodes@qq.com>
2024-07-08 14:43:22 +0800

    updata

--- api/driftBottle/index.js
+++
@@ -1,7 +0,0 @@
-import request from "@/utils/request/index"
-
-export const createDriftBottle = (data) => request({ url: '/driftBottle/createDriftBottle', method: 'post', data })
-
-export const getRandomDriftBottle = () => request({ url: '/driftBottle/getRandomDriftBottle', method: 'get' })
-
-export const updataDriftBottle = (data) => request({ url: '/driftBottle/updataDriftBottle', method: 'post', data })
