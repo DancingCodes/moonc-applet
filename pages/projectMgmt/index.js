f0f46318f3769c39ff1e168f3894d1be51808497
dancingcodes <dancingcodes@qq.com>
2024-09-25 11:27:25 +0800

    使用组件

--- pages/projectMgmt/index.js
+++ pages/projectMgmt/index.js
@@ -3,10 +3,42 @@ import { toast } from '@/utils/toast/index'
 
 Page({
     data: {
-        content: ''
+        // 内容
+        content: '',
+        // 订阅时间
+        currentDate: '',
+        // 时间选择器展示状态
+        timePickerShow: false
     },
     onLoad() {
     },
+    // 打开时间选择器
+    showTimePicker() {
+        this.setData({
+            timePickerShow: true
+        })
+    },
+    // 关闭时间选择器
+    closeTimePicker() {
+        this.setData({
+            timePickerShow: false
+        })
+    },
+    // 确认时间选择器
+    onTimePickerConfirm(e) {
+        this.setData({
+            currentDate: e.detail
+        })
+        this.closeTimePicker()
+    },
+    // 取消时间选择器
+    onTimePickerCancel() {
+        this.setData({
+            currentDate: ''
+        })
+        this.closeTimePicker()
+    },
+    // 发布
     submit() {
         if (this.data.content.trim().length === 0) {
             return toast('内容不可为空')
@@ -14,12 +46,5 @@ Page({
         createMemoItem({ content: this.data.content }).then(() => {
             toast('添加成功')
         })
-    },
-    onConfirm(e) {
-        console.log(e.detail);
-    },
-    onCancel() {
-        console.log('关闭');
-
     },
 })
